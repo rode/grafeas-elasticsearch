2bc4dd183101a7af6f15ff7d5009731e60a27145
Ahmed Alsabag <ahmedalsabag@ahmeds-mbp.home>
2020-11-24 13:43:49 -0600

    Added error checking and converted tests to basic gotests

--- go/v1beta1/storage/filtering/filtering.go
+++ go/v1beta1/storage/filtering/filtering.go
@@ -5,8 +5,11 @@ import (
 
 	"github.com/google/cel-go/common"
 	"github.com/google/cel-go/parser"
+	"go.uber.org/zap"
 	expr "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
 )
+
+var parseLogger *zap.Logger
 
 // Query holds a parent query that carries the entire search query
 type Query struct {
@@ -48,8 +51,12 @@ func checkIfMustOrShould(function string) string {
 // ParseExpressionEntrypoint will serve as the entrypoint to the filter
 // that is eventually passed to parseExpression which will handle the recursive logic
 func ParseExpressionEntrypoint(filter string) string {
-	parsedExpr, _ := parser.Parse(common.NewStringSource(filter, ""))
-	// function := parsedExpr.GetExpr().GetCallExpr().GetFunction()
+
+	parsedExpr, err := parser.Parse(common.NewStringSource(filter, ""))
+	if len(err.GetErrors()) > 0 {
+		return "Bad Filter"
+	}
+
 	expression := parsedExpr.GetExpr()
 	query := &Query{
 		Query: map[string]interface{}{},
@@ -85,68 +92,64 @@ func ParseExpressionEntrypoint(filter string) string {
 // ParseExpression to parse and create a query
 func parseExpression(expression *expr.Expr) interface{} {
 	var term *Term
+	function := expression.GetCallExpr().GetFunction() // =
 
-	_, isCallExpr := expression.ExprKind.(*expr.Expr_CallExpr)
-	if isCallExpr {
-		function := expression.GetCallExpr().GetFunction() // =
+	// Determine if left and right side are final and if so formulate query
+	leftarg := expression.GetCallExpr().Args[0]
+	rightarg := expression.GetCallExpr().Args[1]
 
-		// Determine if left and right side are final and if so formulate query
-		leftarg := expression.GetCallExpr().Args[0]
-		rightarg := expression.GetCallExpr().Args[1]
+	// Check to see if each side is an identity or const expression
+	_, leftIsIdent := leftarg.ExprKind.(*expr.Expr_IdentExpr)
+	_, leftIsConst := leftarg.ExprKind.(*expr.Expr_ConstExpr)
+	_, rightIsIdent := rightarg.ExprKind.(*expr.Expr_IdentExpr)
+	_, rightIsConst := rightarg.ExprKind.(*expr.Expr_ConstExpr)
 
-		// Check to see if each side is an identity or const expression
-		_, leftIsIdent := leftarg.ExprKind.(*expr.Expr_IdentExpr)
-		_, leftIsConst := leftarg.ExprKind.(*expr.Expr_ConstExpr)
-		_, rightIsIdent := rightarg.ExprKind.(*expr.Expr_IdentExpr)
-		_, rightIsConst := rightarg.ExprKind.(*expr.Expr_ConstExpr)
+	// operationalTerm may hold a Must or Should
+	var operationalTerm interface{}
+	if checkIfMustOrShould(function) == "must" {
+		var mustTerm []interface{}
+		//append left recursively and add to the must slice
+		mustTerm = append(mustTerm, parseExpression(leftarg))
+		//append right recursively and add to the must slice
+		mustTerm = append(mustTerm, parseExpression(rightarg))
 
-		// operationalTerm may hold a Must or Should
-		var operationalTerm interface{}
-		if checkIfMustOrShould(function) == "must" {
-			var mustTerm []interface{}
-			//append left recursively
-			mustTerm = append(mustTerm, parseExpression(leftarg))
-			//append right recursively
-			mustTerm = append(mustTerm, parseExpression(rightarg))
-
-			operationalTerm = &Must{
-				Must: mustTerm,
-			}
-			return &Bool{Bool: operationalTerm}
-
-		} else if checkIfMustOrShould(function) == "should" {
-			var shouldTerm []interface{}
-			shouldTerm = append(shouldTerm, parseExpression(leftarg))
-			shouldTerm = append(shouldTerm, parseExpression(rightarg))
-			operationalTerm = &Should{
-				Should: shouldTerm,
-			}
-			return &Bool{Bool: operationalTerm}
-
-		} else if checkIfMustOrShould(function) == "equal" {
-			leftString := ""
-			rightString := ""
-			if leftIsIdent && rightIsConst {
-				leftString = leftarg.GetIdentExpr().Name
-				rightString = rightarg.GetConstExpr().GetStringValue()
-			} else if leftIsConst && rightIsConst {
-				leftString = leftarg.GetConstExpr().GetStringValue()
-				rightString = rightarg.GetConstExpr().GetStringValue()
-			} else if leftIsIdent && rightIsIdent {
-				leftString = leftarg.GetIdentExpr().Name
-				rightString = rightarg.GetIdentExpr().Name
-			} else if leftIsConst && rightIsIdent {
-				leftString = leftarg.GetConstExpr().GetStringValue()
-				rightString = rightarg.GetIdentExpr().Name
-			}
-
-			term = &Term{
-				Term: map[string]string{
-					leftString: rightString,
-				},
-			}
-
+		operationalTerm = &Must{
+			Must: mustTerm,
 		}
+		return &Bool{Bool: operationalTerm}
+
+	} else if checkIfMustOrShould(function) == "should" {
+		var shouldTerm []interface{}
+		shouldTerm = append(shouldTerm, parseExpression(leftarg))
+		shouldTerm = append(shouldTerm, parseExpression(rightarg))
+		operationalTerm = &Should{
+			Should: shouldTerm,
+		}
+		return &Bool{Bool: operationalTerm}
+
+	} else if checkIfMustOrShould(function) == "equal" {
+		var leftString string
+		var rightString string
+		if leftIsIdent && rightIsConst {
+			leftString = leftarg.GetIdentExpr().Name
+			rightString = rightarg.GetConstExpr().GetStringValue()
+		} else if leftIsConst && rightIsConst {
+			leftString = leftarg.GetConstExpr().GetStringValue()
+			rightString = rightarg.GetConstExpr().GetStringValue()
+		} else if leftIsIdent && rightIsIdent {
+			leftString = leftarg.GetIdentExpr().Name
+			rightString = rightarg.GetIdentExpr().Name
+		} else if leftIsConst && rightIsIdent {
+			leftString = leftarg.GetConstExpr().GetStringValue()
+			rightString = rightarg.GetIdentExpr().Name
+		}
+
+		term = &Term{
+			Term: map[string]string{
+				leftString: rightString,
+			},
+		}
+
 	}
 	return term
 }
--- go/v1beta1/storage/filtering/filtering_test.go
+++ go/v1beta1/storage/filtering/filtering_test.go
@@ -1,157 +1,84 @@
 package filtering
 
-import (
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-)
+import "testing"
 
-var _ = Describe("filtering queries", func() {
+func TestSingleTerm(t *testing.T) {
+	filter := `a=="b"`
+	expectedResult := `{"query":{"bool":{"must":[{"term":{"a":"b"}}]}}}`
+	actualResult := ParseExpressionEntrypoint(filter)
+	if actualResult != expectedResult {
+		t.Errorf("Expected: %v \n Received: %v", expectedResult, actualResult)
+	}
+}
 
-	Context("Performing AND queries", func() {
+func TestSingleTermLeftConstRightConst(t *testing.T) {
+	filter := `"a"=="b"`
+	expectedResult := `{"query":{"bool":{"must":[{"term":{"a":"b"}}]}}}`
+	actualResult := ParseExpressionEntrypoint(filter)
+	if actualResult != expectedResult {
+		t.Errorf("Expected: %v \n Received: %v", expectedResult, actualResult)
+	}
+}
 
-		When("when use a single TERM", func() {
-			var (
-				filter         string
-				expectedResult string
-				actualResult   string
-			)
-			BeforeEach(func() {
-				filter = "a==\"b\""
-				expectedResult = `{"query":{"bool":{"must":[{"term":{"a":"b"}}]}}}`
-				actualResult = ParseExpressionEntrypoint(filter)
+func TestSingleTermLeftIdentRightIdent(t *testing.T) {
+	filter := "a==b"
+	expectedResult := `{"query":{"bool":{"must":[{"term":{"a":"b"}}]}}}`
+	actualResult := ParseExpressionEntrypoint(filter)
+	if actualResult != expectedResult {
+		t.Errorf("Expected: %v \n Received: %v", expectedResult, actualResult)
+	}
+}
 
-			})
+func TestSingleTermLeftConstRightIdent(t *testing.T) {
+	filter := `"a"==b`
+	expectedResult := `{"query":{"bool":{"must":[{"term":{"a":"b"}}]}}}`
+	actualResult := ParseExpressionEntrypoint(filter)
+	if actualResult != expectedResult {
+		t.Errorf("Expected: %v \n Received: %v", expectedResult, actualResult)
+	}
+}
 
-			It("should return a singular elastic query json", func() {
-				Expect(expectedResult).To(BeIdenticalTo(actualResult))
-			})
-		})
+func TestAndingTwoTerm(t *testing.T) {
+	filter := `(a=="b")&&(c=="d")`
+	expectedResult := `{"query":{"bool":{"must":[{"term":{"a":"b"}},{"term":{"c":"d"}}]}}}`
+	actualResult := ParseExpressionEntrypoint(filter)
+	if actualResult != expectedResult {
+		t.Errorf("Expected: %v \n Received: %v", expectedResult, actualResult)
+	}
+}
 
-		When("when use a single TERM with a left const and right const", func() {
-			var (
-				filter         string
-				expectedResult string
-				actualResult   string
-			)
-			BeforeEach(func() {
-				filter = "\"a\"==\"b\""
-				expectedResult = `{"query":{"bool":{"must":[{"term":{"a":"b"}}]}}}`
-				actualResult = ParseExpressionEntrypoint(filter)
+func TestAndingThreeTerms(t *testing.T) {
+	filter := `(a=="b")&&(c=="d")&&(e=="f")`
+	expectedResult := `{"query":{"bool":{"must":[{"bool":{"must":[{"term":{"a":"b"}},{"term":{"c":"d"}}]}},{"term":{"e":"f"}}]}}}`
+	actualResult := ParseExpressionEntrypoint(filter)
+	if actualResult != expectedResult {
+		t.Errorf("Expected: %v \n Received: %v", expectedResult, actualResult)
+	}
+}
 
-			})
+func TestAndingTermWithOrSet(t *testing.T) {
+	filter := `(a=="b")&&((c=="d")||(e=="f"))`
+	expectedResult := `{"query":{"bool":{"must":[{"term":{"a":"b"}},{"bool":{"should":[{"term":{"c":"d"}},{"term":{"e":"f"}}]}}]}}}`
+	actualResult := ParseExpressionEntrypoint(filter)
+	if actualResult != expectedResult {
+		t.Errorf("Expected: %v \n Received: %v", expectedResult, actualResult)
+	}
+}
 
-			It("should return a singular elastic query json", func() {
-				Expect(expectedResult).To(BeIdenticalTo(actualResult))
-			})
-		})
+func TestAndingAndSetWithOrSet(t *testing.T) {
+	filter := `((a=="b")&&(g=="h")) && ((c=="d")||(e=="f"))`
+	expectedResult := `{"query":{"bool":{"must":[{"bool":{"must":[{"term":{"a":"b"}},{"term":{"g":"h"}}]}},{"bool":{"should":[{"term":{"c":"d"}},{"term":{"e":"f"}}]}}]}}}`
+	actualResult := ParseExpressionEntrypoint(filter)
+	if actualResult != expectedResult {
+		t.Errorf("Expected: %v \n Received: %v", expectedResult, actualResult)
+	}
+}
 
-		When("when use a single TERM with a left ident and right ident", func() {
-			var (
-				filter         string
-				expectedResult string
-				actualResult   string
-			)
-			BeforeEach(func() {
-				filter = "a==b"
-				expectedResult = `{"query":{"bool":{"must":[{"term":{"a":"b"}}]}}}`
-				actualResult = ParseExpressionEntrypoint(filter)
-
-			})
-
-			It("should return a singular elastic query json", func() {
-				Expect(expectedResult).To(BeIdenticalTo(actualResult))
-			})
-		})
-
-		When("when use a single TERM with a left const and right ident", func() {
-			var (
-				filter         string
-				expectedResult string
-				actualResult   string
-			)
-			BeforeEach(func() {
-				filter = "\"a\"==b"
-				expectedResult = `{"query":{"bool":{"must":[{"term":{"a":"b"}}]}}}`
-				actualResult = ParseExpressionEntrypoint(filter)
-
-			})
-
-			It("should return a singular elastic query json", func() {
-				Expect(expectedResult).To(BeIdenticalTo(actualResult))
-			})
-		})
-
-		When("when ANDing two TERMs", func() {
-			var (
-				filter         string
-				expectedResult string
-				actualResult   string
-			)
-			BeforeEach(func() {
-				filter = "(a==\"b\")&&(c==\"d\")"
-				expectedResult = `{"query":{"bool":{"must":[{"term":{"a":"b"}},{"term":{"c":"d"}}]}}}`
-				actualResult = ParseExpressionEntrypoint(filter)
-
-			})
-
-			It("should return a singular elastic query json", func() {
-				Expect(expectedResult).To(BeIdenticalTo(actualResult))
-			})
-		})
-
-		When("when ANDing three TERMs", func() {
-			var (
-				filter         string
-				expectedResult string
-				actualResult   string
-			)
-			BeforeEach(func() {
-				filter = "(a==\"b\")&&(c==\"d\")&&(e==\"f\")"
-				expectedResult = `{"query":{"bool":{"must":[{"bool":{"must":[{"term":{"a":"b"}},{"term":{"c":"d"}}]}},{"term":{"e":"f"}}]}}}`
-				actualResult = ParseExpressionEntrypoint(filter)
-
-			})
-
-			It("should return a singular elastic query json", func() {
-				Expect(expectedResult).To(BeIdenticalTo(actualResult))
-			})
-		})
-
-		When("when ANDing a term and an OR set", func() {
-			var (
-				filter         string
-				expectedResult string
-				actualResult   string
-			)
-			BeforeEach(func() {
-				filter = "(a==\"b\")&&((c==\"d\")||(e==\"f\"))"
-				expectedResult = `{"query":{"bool":{"must":[{"term":{"a":"b"}},{"bool":{"should":[{"term":{"c":"d"}},{"term":{"e":"f"}}]}}]}}}`
-				actualResult = ParseExpressionEntrypoint(filter)
-
-			})
-
-			It("should return a singular elastic query json", func() {
-				Expect(expectedResult).To(BeIdenticalTo(actualResult))
-			})
-		})
-
-		When("when ANDing an AND set and an OR set", func() {
-			var (
-				filter         string
-				expectedResult string
-				actualResult   string
-			)
-			BeforeEach(func() {
-				filter = "((a==\"b\")&&(g==\"h\")) && ((c==\"d\")||(e==\"f\"))"
-				expectedResult = `{"query":{"bool":{"must":[{"bool":{"must":[{"term":{"a":"b"}},{"term":{"g":"h"}}]}},{"bool":{"should":[{"term":{"c":"d"}},{"term":{"e":"f"}}]}}]}}}`
-				actualResult = ParseExpressionEntrypoint(filter)
-
-			})
-
-			It("should return a singular elastic query json", func() {
-				Expect(expectedResult).To(BeIdenticalTo(actualResult))
-			})
-		})
-
-	})
-})
+func TestBadFilter(t *testing.T) {
+	filter := `a==`
+	expectedResult := "Bad Filter"
+	actualResult := ParseExpressionEntrypoint(filter)
+	if actualResult != expectedResult {
+		t.Errorf("Expected: %v \n Received: %v", expectedResult, actualResult)
+	}
+}

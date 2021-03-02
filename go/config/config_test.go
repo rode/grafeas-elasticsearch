e52901ba95a894f1bc16a103d218b9f23c8da010
Ahmed Alsabag <ahmedalsabag@ahmeds-mbp.home>
2021-03-02 12:57:59 -0600

    adding tests for setting a field in a config file

--- go/config/config_test.go
+++ go/config/config_test.go
@@ -47,4 +47,19 @@ var _ = Describe("ElasticsearchConfig", func() {
 			Refresh: "somethingInvalid",
 		}, true),
 	)
+
+	When("setting the InsecureSkipVerify boolean value", func() {
+		It("shoulbe be true when set to true", func() {
+			abc := &ElasticsearchConfig{
+				InsecureSkipVerify: true,
+			}
+			Expect(abc.InsecureSkipVerify).Should(BeTrue())
+		})
+		It("shoulbe be false when set to false", func() {
+			abc := &ElasticsearchConfig{
+				InsecureSkipVerify: false,
+			}
+			Expect(abc.InsecureSkipVerify).ShouldNot(BeTrue())
+		})
+	})
 })
